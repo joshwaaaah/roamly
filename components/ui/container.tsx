933179adf1c3ff8496e26d7c91a431feeb892f98
jnhartshorne@gmail.com <jnhartshorne@gmail.com>
2026-01-01 13:03:28 +0000

    Add basic container component

---
+++ components/ui/container.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, View } from 'react-native';
+
+type Props = {
+  className?: string
+  children: React.ReactNode,
+}
+
+export function Container({ children, className }: Props) {
+  return (
+    <View style={styles.container} className={className}>
+      {children}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+  },
+});
