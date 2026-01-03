345418fd364ab42ef6780dee42980dec6299a2c2
jnhartshorne@gmail.com <jnhartshorne@gmail.com>
2026-01-03 00:36:00 +0000

    Add loading prop

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,13 +1,31 @@
-import { Pressable, type PressableProps, Text } from 'react-native';
+import {
+  Pressable,
+  type PressableProps,
+  Text,
+  View,
+  ActivityIndicator,
+} from 'react-native';
 
 type Props = PressableProps & {
   title: string;
+  loading?: boolean;
 };
 
 export function Button(props: Props) {
+  const { loading, title, ...rest } = props;
   return (
-    <Pressable {...props} className="bg-black p-5 rounded-xl">
-      <Text className="text-center text-white text-lg">{props.title}</Text>
+    <Pressable {...rest} className="bg-black p-5 rounded-xl">
+      <Text
+        className={`text-center text-white text-lg ${loading ? 'opacity-0' : ''}`}
+      >
+        {title}
+      </Text>
+
+      {loading && (
+        <View className="absolute inset-0 flex items-center justify-center">
+          <ActivityIndicator size="small" color="#ffffff" />
+        </View>
+      )}
     </Pressable>
   );
 }
